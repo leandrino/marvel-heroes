1ea9aad6058942bf1f20ae74b747b29272f9eeda
Leandro Alexandrino <alexandrinowd@gmail.com>
2018-10-06 16:32:42 -0300

    create papaer default common component

---
+++ frontend/src/commons/PaperDefault/PaperDefault.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper/Paper";
+
+const style = theme => ({
+  root: {
+    margin: theme.spacing.unit * 4
+  }
+});
+
+const PaperDefault = ({ classes, children }) => (
+  <Paper className={classes.root}>
+    {children}
+  </Paper>
+);
+
+export default withStyles(style)(PaperDefault);
